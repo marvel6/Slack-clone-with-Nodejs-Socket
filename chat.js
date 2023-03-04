fa719898243c0e4f9e00c27c9c9da8a7a3aad63e
marvel6 <marvelloussolomon49@gmail.com>
2023-03-05 00:25:17 +0100

    new commit

---
+++ chat.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const socketio = require('socket.io')
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(9000);
+const io = socketio(expressServer);
+
+
+const Namespace = require('./data/namespaces')
+
+
+
+io.on('connection',(socket)=>{
+
+  
+
+})
+
+
+Namespace.forEach(name => {
+    io.of(name.endpoint).on('connection',(socket) => {
+        console.log(`${socket.id} just joined this endpoint ${name.endpoint}`)
+    })
+})
